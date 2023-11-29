90e2fd4595c25b32bb9e6b488a7d646a19995deb
abiola <abiole68@gmail.com>
2023-11-29 12:03:22 +0000

    12:03 29 11 2023

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
- import Home from "./Screens/Home";
- import Services from './Screens/Services';
- import Contact from './Screens/Contact';
- import { Route, Routes, NavLink } from 'react-router-dom';
+import Home from "./Screens/Home";
+import Services from './Screens/Services';
+import Contact from './Screens/Contact';
+import { Route, Routes, NavLink } from 'react-router-dom';
 
 function App() {
   return (
     <div className='container mt-4'>
       <nav className='mb-10 flex items-center justify-between'>
         <div>
-          <NavLink to="/" className="mr-10 font-extrabold" style={({isActive}) => ({fontWeight : isActive ? 'bold' : 'normal'})}>Home</NavLink>
-          <NavLink to="/services" className="mr-10 font-extrabold" style={({isActive}) => ({fontWeight : isActive ? 'bold' : 'normal'})}>Services</NavLink>
-          <NavLink to="/contact" className="mr-10 font-extrabold" style={({isActive}) => ({fontWeight : isActive ? 'bold' : 'normal'})}>Contact</NavLink>
+          <NavLink to="/" className="mr-10 font-extrabold" style={({ isActive }) => ({ fontWeight: isActive ? 'bold' : 'normal' })}>Home</NavLink>
+          <NavLink to="/services" className="mr-10 font-extrabold" style={({ isActive }) => ({ fontWeight: isActive ? 'bold' : 'normal' })}>Services</NavLink>
+          <NavLink to="/contact" className="mr-10 font-extrabold" style={({ isActive }) => ({ fontWeight: isActive ? 'bold' : 'normal' })}>Contact</NavLink>
         </div>
         <NavLink to="/"><img src="./image/nameFioFood.png" alt="logo du site" width="100" /></NavLink>
       </nav>
